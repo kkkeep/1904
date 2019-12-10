88717dc99311d6afa38c79a4a3a868c85afb3d12
taofu <taofujob@gmail.com>
2019-12-10 17:11:13 +0800

    自定义view2

--- libcustomview2/src/main/java/com/jy/libcustomview2/SliderView.java
+++ libcustomview2/src/main/java/com/jy/libcustomview2/SliderView.java
@@ -1,15 +1,15 @@
 package com.jy.libcustomview2;
 
-import android.animation.ValueAnimator;
 import android.content.Context;
 import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Rect;
-import android.graphics.Typeface;
+import android.graphics.RectF;
 import android.util.AttributeSet;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -24,10 +24,17 @@ import java.util.ArrayList;
  **/
 public class SliderView extends View {
 
-    private static final String  [] CHARS = {"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","#"};
+    private static final String[] CHARS = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "#"};
     private Paint mPaint;
-    private Paint mGridPaint;
+    private Paint mStrokePaint;
+    private Paint mBgPaint;
+    private Paint mSelectedCharBgPaint;
     private ArrayList<Char> mChars;
+
+    private Char mPreChar;
+
+    private RectF mFillRect;
+    private RectF mStrokeRect;
 
     private float mTextSize;
 
@@ -43,18 +50,18 @@ public class SliderView extends View {
     private int mTextColor;
     private int mGridHeight;
 
-
+    private float mStrokeWidth;
 
 
     private boolean isSetWH = false;
 
 
     public SliderView(Context context) {
-        this(context,null);
+        this(context, null);
     }
 
     public SliderView(Context context, @Nullable AttributeSet attrs) {
-        this(context, attrs,0);
+        this(context, attrs, 0);
     }
 
     public SliderView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
@@ -62,11 +69,11 @@ public class SliderView extends View {
 
         TypedArray typedArray = getContext().obtainStyledAttributes(attrs, R.styleable.SliderView);
 
-        mTextSize = typedArray.getDimension(R.styleable.SliderView_sliderTextSize,dp2px(context, 15.0f));
-        mMarginLeft = typedArray.getDimension(R.styleable.SliderView_sliderCharLeftMargin,dp2px(context, 12.0f));
-        mMarginRight = typedArray.getDimension(R.styleable.SliderView_sliderCharRightMargin,dp2px(context, 12.0f));
-        mMarginTop = typedArray.getDimension(R.styleable.SliderView_sliderCharTopMargin,dp2px(context, 12.0f));
-        mMarginBottom = typedArray.getDimension(R.styleable.SliderView_sliderCharBottomMargin,dp2px(context, 12.0f));
+        mTextSize = typedArray.getDimension(R.styleable.SliderView_sliderTextSize, dp2px(context, 15.0f));
+        mMarginLeft = typedArray.getDimension(R.styleable.SliderView_sliderCharLeftMargin, dp2px(context, 12.0f));
+        mMarginRight = typedArray.getDimension(R.styleable.SliderView_sliderCharRightMargin, dp2px(context, 12.0f));
+        mMarginTop = typedArray.getDimension(R.styleable.SliderView_sliderCharTopMargin, dp2px(context, 12.0f));
+        mMarginBottom = typedArray.getDimension(R.styleable.SliderView_sliderCharBottomMargin, dp2px(context, 12.0f));
         mTextColor = typedArray.getColor(R.styleable.SliderView_sliderTextColor, Color.BLACK);
 
         typedArray.recycle();
@@ -83,10 +90,9 @@ public class SliderView extends View {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
 
 
-
-        ViewGroup  viewGroup = (ViewGroup) getParent();
-        if(viewGroup instanceof ConstraintLayout){
-            if(!isSetWH){
+        ViewGroup viewGroup = (ViewGroup) getParent();
+        if (viewGroup instanceof ConstraintLayout) {
+            if (!isSetWH) {
                 ConstraintLayout parent = (ConstraintLayout) viewGroup;
                 ConstraintSet constraintSet = new ConstraintSet();
                 constraintSet.clone(parent);
@@ -95,8 +101,8 @@ public class SliderView extends View {
                 constraintSet.applyTo(parent);
                 isSetWH = true;
             }
-        }else{
-            setMeasuredDimension(MeasureSpec.makeMeasureSpec(mWidth, MeasureSpec.EXACTLY),MeasureSpec.makeMeasureSpec(mHeight, MeasureSpec.EXACTLY));
+        } else {
+            setMeasuredDimension(MeasureSpec.makeMeasureSpec(mWidth, MeasureSpec.EXACTLY), MeasureSpec.makeMeasureSpec(mHeight, MeasureSpec.EXACTLY));
         }
     }
 
@@ -109,17 +115,101 @@ public class SliderView extends View {
 
     @Override
     protected void onDraw(Canvas canvas) {
-        super.onDraw(canvas);
-        for(Char aChar : mChars){
+        ///super.onDraw(canvas);
+
+        // canvas.drawColor(Color.GRAY);
+
+
+
+        //}
+
+        canvas.drawRoundRect(mStrokeRect, mStrokeRect.width() / 2, mStrokeRect.width() / 2, mStrokePaint);
+        canvas.drawRoundRect(mFillRect, mFillRect.width() / 2, mFillRect.width() / 2, mBgPaint);
+
+
+        for (Char aChar : mChars) {
+            if(aChar.isSelect){
+                canvas.drawOval(aChar.getSelectRectF(), mSelectedCharBgPaint);
+            }
             canvas.drawText(aChar.value, aChar.x, aChar.y, mPaint);
-           // canvas.drawRect(aChar.rect, mGridPaint);
+
         }
+
+
+
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+
+        Log.d("Test", "touch " + event.getAction() + " x = " + event.getX()  + " y = " + event.getY());
+        switch (event.getAction()){
+
+            case MotionEvent.ACTION_DOWN:{
+                Char value = getValue(event.getX(), event.getY());
+                if(mPreChar != null){
+                    mPreChar.isSelect = false;
+                }
+                mPreChar = value;
+
+                if(value != null){
+                    invalidate();
+                }
+                break;
+            }
+
+            case MotionEvent.ACTION_MOVE:{
+
+                Char value = getValue(event.getX(), event.getY());
+                if(value != null && value != mPreChar){
+                    if(mPreChar != null){
+                        mPreChar.isSelect = false;
+                    }
+                    mPreChar = value;
+                    invalidate();
+                }
+
+                break;
+            }
+
+            case MotionEvent.ACTION_UP:{
+
+                if(mPreChar != null){
+                    mPreChar.isSelect = false;
+                    mPreChar = null;
+                }
+                invalidate();
+
+                break;
+            }
+
+        }
+
+        return true;
     }
 
 
+    /**
+     * 获取 down 和 move 的点在 27个格子中对应的 字母
+     * @param x
+     * @param y
+     * @return
+     */
+    private Char getValue(float x, float y) {
+        Char value = null;
+        for (Char c : mChars) {
+            if (c.rect.contains(x, y)) {
+                value = c;
+                c.isSelect = true;
+                break;
+            } else {
+                c.isSelect = false;
 
-
-    private void init(){
+            }
+        }
+        return value;
+    }
+    private void init() {
 
         mPaint = new Paint();
 
@@ -128,53 +218,112 @@ public class SliderView extends View {
         mPaint.setAntiAlias(true);
         mPaint.setTextAlign(Paint.Align.CENTER);
 
-        mGridPaint = new Paint();
-        mGridPaint.setStrokeWidth(3);
-        mGridPaint.setStyle(Paint.Style.STROKE);
 
-        mCharWidth =  mPaint.measureText("W"); // 计算文字的宽度
+        mStrokeWidth = 1.5f;
+        mStrokePaint = new Paint();
+        mStrokePaint.setAntiAlias(true);
+        mStrokePaint.setStrokeWidth(mStrokeWidth);
+        mStrokePaint.setStyle(Paint.Style.STROKE);
+        mStrokePaint.setColor(Color.BLACK);
+
+
+        mBgPaint = new Paint();
+        mBgPaint.setStrokeWidth(1);
+        mBgPaint.setAntiAlias(true);
+        mBgPaint.setStyle(Paint.Style.FILL);
+        mBgPaint.setColor(Color.WHITE);
+
+        mSelectedCharBgPaint = new Paint();
+        mSelectedCharBgPaint.setStrokeWidth(1);
+        mSelectedCharBgPaint.setAntiAlias(true);
+        mSelectedCharBgPaint.setStyle(Paint.Style.FILL);
+        mSelectedCharBgPaint.setColor(Color.RED);
+
+
+
+
+
+        mCharWidth = mPaint.measureText("W"); // 计算文字的宽度
 
         mWidth = (int) (mMarginLeft + mCharWidth + mMarginRight);
 
-        Paint.FontMetrics fontMetrics =  mPaint.getFontMetrics();
-        mCharHeight = Math.min(mTextSize,Math.abs(fontMetrics.ascent));
+        Paint.FontMetrics fontMetrics = mPaint.getFontMetrics();
+
+
+        mCharHeight = Math.min(mTextSize, Math.abs(fontMetrics.ascent));
 
         mGridHeight = (int) (mMarginTop + mCharHeight + mMarginBottom);
 
-        mHeight = (int) (CHARS.length * (mCharHeight + mMarginBottom + mMarginTop));
+        mHeight = (int) (CHARS.length * mGridHeight);
 
+        Log.d("Test", "------ height " + mHeight);
         mChars = new ArrayList<>(CHARS.length);
 
+        mStrokeRect = new RectF();
+
+        mStrokeRect.left = mStrokeWidth / 2;
+        mStrokeRect.top = mStrokeWidth / 2;
+        mStrokeRect.right = mWidth - mStrokeWidth / 2;
+        mStrokeRect.bottom = mHeight - mStrokeWidth / 2;
+
+        mFillRect = new RectF();
+        mFillRect.left = mStrokeRect.left + mStrokeWidth / 2;
+        mFillRect.top =  mStrokeRect.top+ mStrokeWidth / 2;
+        mFillRect.right =   mStrokeRect.right - mStrokeWidth / 2;
+        mFillRect.bottom = mStrokeRect.bottom - mStrokeWidth / 2;
+
+        //mFillRect = mStrokeRect;
+
         Char aChar;
-        for(int i =0; i < CHARS.length;i++){
-            aChar = new Char(CHARS[i],i);
+        for (int i = 0; i < CHARS.length; i++) {
+            aChar = new Char(CHARS[i], i);
             mChars.add(aChar);
         }
-
 
     }
 
 
     private class Char {
         String value;
-        Rect rect  = new Rect();
+        RectF rect = new RectF();
         float y;
         float x;
+        RectF select;
+        int position;
+        boolean isSelect;
 
-        public Char(String value,int position) {
+        public Char(String value, int position) {
             this.value = value;
+            this.position = position;
             rect.top = position * mGridHeight;
             rect.left = 0;
             rect.right = mWidth;
             rect.bottom = rect.top + mGridHeight;
 
-            y = ((rect.height() /2) + (mCharHeight /2)) + rect.top;
-            x = rect.width() /2;
+            y = ((rect.height() / 2) + (mCharHeight / 2)) + rect.top;
+            x = rect.width() / 2;
 
-
-
-            Log.d("Test", value + " x = " + x + " y = " + y);
+            Log.d("Test", value + " x = " + x + " y = " + rect.bottom);
         }
+
+        public RectF getSelectRectF(){
+            if(select == null){
+                int offset = (int) Math.min(mStrokeWidth * 4,rect.width() / 10);
+
+                int bottom = (int) (rect.bottom - offset + mCharHeight * 0.20);
+
+                if(position == CHARS.length -1){
+                    bottom = (int) (rect.bottom - offset);
+                }
+
+                select = new RectF(rect.left + offset,rect.top + offset,rect.right - offset,bottom );
+
+            }
+
+            return select;
+        }
+
+
     }
 
 
@@ -182,9 +331,6 @@ public class SliderView extends View {
         float scale = context.getResources().getDisplayMetrics().density;
         return (int) (dp * scale + 0.5f);
     }
-
-
-
 
 
     private String getModeName(int mode) {
